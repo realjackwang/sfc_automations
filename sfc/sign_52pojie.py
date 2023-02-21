7cbaaf9d50c293c1009751f823a8391d35f1c8d2
Jack Wang <realjackwang@gmail.com>
2023-02-21 14:16:43 +0800

    Create sign_52pojie.py

---
+++ sfc/sign_52pojie.py
@@ -0,0 +1,14 @@
+import os
+
+
+def main():
+    title = '吾爱破解签到-测试'
+    success = True
+
+    os.environ['pojie_cookie'] = 2
+    return [title, success]
+
+
+if __name__ == '__main__':
+  main()
+ 
